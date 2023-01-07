32c7652572ca40f2159b557df00e28327de27e0e
Waldemar <karasev.vladimir.2000@gmail.com>
2023-01-07 16:15:32 +0300

    Added Renderer folder and Implement ShaderProgram class

---
+++ src/Renderer/ShaderProgram.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include <glad/glad.h>
+#include <string>
+
+namespace Renderer {
+	class ShaderProgram {
+	public:
+		ShaderProgram() = delete;
+		ShaderProgram(ShaderProgram& shaderProgram) = delete;
+
+		ShaderProgram(const std::string& vertexShader, const std::string& fragmentShader);
+		
+		// Move constructor
+		ShaderProgram(ShaderProgram&& shaderProgram) noexcept;
+
+
+		~ShaderProgram();
+		
+		// return flag of shader compile????
+		bool isCompiled() const { return m_isCompiled; }
+
+		// Shader enamle function
+		void use() const;
+
+		// Copy assignment is forbidden
+		ShaderProgram& operator=(const ShaderProgram& shaderProgram) = delete;
+
+		// Move assignment
+		ShaderProgram& operator=(ShaderProgram&& shaderProgram) noexcept;
+		
+
+	private:
+		// Create Shader
+		bool createShader(const std::string& source, const GLenum shaderTpe, GLuint& shaderID);
+
+		// Did shader compile successfully? flag
+		bool m_isCompiled = false;
+		
+		// ID of shader program
+		GLuint m_ID = 0;
+
+	};
+}
--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,8 @@
 
 #include <iostream>
 
+#include "Renderer/ShaderProgram.h"
+
 GLfloat point[] = {
      0.0f,  0.5f, 0.0f,
      0.5f, -0.5f, 0.0f,
@@ -104,30 +106,14 @@ int main(void)
     
     glClearColor(1, 1, 0, 1);    
 
-    // Creating shaders
-    // Vertex shader
-    // Shader identifier
-    GLuint vs = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vs, 1, &vertex_shader, nullptr);
-    // Compiling vertex shader
-    glCompileShader(vs);
+    std::string vertexShader(vertex_shader);
+    std::string fragmentShader(fragment_shader);
+    Renderer::ShaderProgram shaderProgram(vertexShader, fragmentShader);
 
-    // Fragment shader
-    // Shader identifier
-    GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fs, 1, &fragment_shader, nullptr);
-    // Compiling fragment shader
-    glCompileShader(fs);
-
-    // Linking vertex and fragment shaders
-    GLuint shader_program = glCreateProgram();
-    glAttachShader(shader_program, vs);
-    glAttachShader(shader_program, fs);
-    glLinkProgram(shader_program);
-
-    // Deleting vs and fs
-    glDeleteShader(vs);
-    glDeleteShader(fs);
+    if (!shaderProgram.isCompiled())
+    {
+        std::cerr << "Can't create shader program!" << std::endl;
+    }
 
     // Passing shader_program to video card
     // VBO creating (Vertex Buffer Object)
@@ -173,7 +159,7 @@ int main(void)
         glClear(GL_COLOR_BUFFER_BIT);
 
         // Drawwing triangle
-        glUseProgram(shader_program);
+        shaderProgram.use();
         glBindVertexArray(vao);
         glDrawArrays(GL_TRIANGLES, 0, 3);
 
